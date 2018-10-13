54032eaea93f6be3ac2640866a58286f61b8f16e
mratsim <mamy_github@numforge.co>
2018-10-13 14:44:29 +0200

    Initial commit

---
+++ laser/laser.nim
@@ -0,0 +1,4 @@
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
