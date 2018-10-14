d6b2bf30fe7132a60c6de3ed56f58d9e1517d152
mratsim <mamy_github@numforge.co>
2018-10-15 01:20:32 +0200

    init benchmark of best tensor iteration scheme

---
+++ benchmarks/loop_iteration/README
@@ -0,0 +1,5 @@
+# Benchmark - loop iteration
+
+This compares iteration speed on a variadic number of tensors.
+
+Please also check TRIOT - https://github.com/mratsim/Arraymancer/issues/164
---
+++ benchmarks/loop_iteration/utils.nim
@@ -0,0 +1,35 @@
+# MIT License
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+
+import macros
+
+func product*(x: varargs[int]): int =
+  result = 1
+  for val in x: result *= val
+
+proc replaceNodes*(ast: NimNode, values: NimNode, containers: NimNode): NimNode =
+  # Args:
+  #   - The full syntax tree
+  #   - an array of replacement value
+  #   - an array of identifiers to replace
+  proc inspect(node: NimNode): NimNode =
+    case node.kind:
+    of {nnkIdent, nnkSym}:
+      for i, c in containers:
+        if node.eqIdent($c):
+          return values[i]
+      return node
+    of nnkEmpty: return node
+    of nnkLiterals: return node
+    else:
+      var rTree = node.kind.newTree()
+      for child in node:
+        rTree.add inspect(child)
+      return rTree
+  result = inspect(ast)
+
+proc pop*(tree: var NimNode): NimNode =
+  ## varargs[untyped] consumes all arguments so the actual value should be popped
+  ## https://github.com/nim-lang/Nim/issues/5855
+  result = tree[tree.len-1]
+  tree.del(tree.len-1)
