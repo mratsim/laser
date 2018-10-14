4fff414388a41dffa757b2ca162126dd653b121e
mratsim <mamy_github@numforge.co>
2018-10-14 12:49:33 +0200

    Add omp_parallel_for template

---
+++ laser/openmp/omp_parallel.nim
@@ -0,0 +1,29 @@
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+import ./omp_tuning, ./omp_mangling
+
+template omp_parallel_for*[T](
+      index: untyped,
+      data: openarray[T],
+      omp_threshold: static Natural,
+      use_simd: static bool = true,
+      body: untyped
+      ) =
+  when omp_threshold == 0:
+    when use_simd:
+      for index in `||`(0, data.len - 1, "simd"):
+        body
+    else:
+      for index in 0||(data.len-1):
+        body
+  else:
+    const ompsize_Csymbol = "ompsize_" & omp_suffix(genNew = true)
+    let ompsize {.exportc: "ompsize_" & omp_suffix(genNew = false).} = data.len
+    when use_simd:
+      const omp_annotation = "simd if(" & $ompthreshold & " < " & ompsize_Csymbol & ")"
+    else:
+      const omp_annotation = "if(" & $ompthreshold & " < " & ompsize_Csymbol & ")"
+    for index in `||`(0, data.len - 1, omp_annotation):
+      body
