6379474cc4d00c870ebc3f00fc700645717ce187
mratsim <mamy_github@numforge.co>
2018-10-20 16:38:04 +0200

    Add Serial and parallel variant for both contiguous and strided iteration cases

--- laser/strided_iteration/map_foreach.nim
+++ laser/strided_iteration/map_foreach.nim
@@ -110,7 +110,8 @@ proc initForEach(
       assert `alias0`.shape == `alias_i`.shape
 
 proc forEachContiguousImpl(
-  values, raw_ptrs, size, loopBody, omp_params: NimNode,
+  values, raw_ptrs, size, loopBody: NimNode,
+  use_openmp: static bool, omp_params: NimNode,
   ): NimNode =
   # Build the parallel body of a contiguous iterator
 
@@ -124,21 +125,25 @@ proc forEachContiguousImpl(
                   to_replace = values
                   )
 
-  result = newStmtList()
-  if omp_params.isNil:
-    result.add quote do:
-      omp_parallel_for_default(`index`, `size`):
-          `body`
+  if use_openmp:
+    if omp_params.isNil:
+      result = quote do:
+        omp_parallel_for_default(`index`, `size`):
+            `body`
+    else:
+      let
+        omp_threshold  = omp_params[0]
+        omp_grain_size = omp_params[1]
+        use_simd       = omp_params[2]
+      result = quote do:
+        omp_parallel_for(
+          `index`, `size`,
+          `omp_threshold`, `omp_grain_size`, `use_simd`):
+            `body`
   else:
-    let
-      omp_threshold  = omp_params[0]
-      omp_grain_size = omp_params[1]
-      use_simd       = omp_params[2]
-    result.add quote do:
-      omp_parallel_for(
-        `index`, `size`,
-        `omp_threshold`, `omp_grain_size`, `use_simd`):
-          `body`
+    result = quote do:
+      for `index` in 0 ..< `size`:
+        `body`
 
 proc forEachStridedImpl(
   values, aliases,
@@ -212,7 +217,6 @@ proc forEachStridedImpl(
           `coord`[`k`] = 0
           `apply_backstrides`
 
-  result = newStmtList()
   if use_openmp:
     let
       omp_threshold  =  if omp_params.isNil: newLit OMP_MEMORY_BOUND_THRESHOLD
@@ -223,20 +227,15 @@ proc forEachStridedImpl(
                           omp_params[1].intVal div OMP_NON_CONTIGUOUS_SCALE_FACTOR
                         )
       use_simd       = if omp_params.isNil: newLit true else: omp_params[2]
-    result.add quote do:
+    result = quote do:
       omp_parallel_chunks(
         `size`, `chunk_offset`, `chunk_size`,
         `omp_threshold`, `omp_grain_size`, `use_simd`):
           `stridedBody`
   else:
-    result.add stridedBody
+    result = stridedBody
 
-macro forEachContiguous*(args: varargs[untyped]): untyped =
-  ## Format:
-  ## forEachContiguous x in a, y in b, z in c, (512, 1024, true):
-  ##    x += y * z
-  ## (512, 1024, true) corresponds to omp_threshold, omp_grain_size, use_simd
-  ## from omp_parallel_for
+template forEachContiguousTemplate(use_openmp: static bool){.dirty.} =
   var
     params, loopBody, values, aliases, raw_ptrs: NimNode
     aliases_stmt, raw_ptrs_stmt, test_shapes: NimNode
@@ -253,8 +252,8 @@ macro forEachContiguous*(args: varargs[untyped]): untyped =
   )
 
   let size = genSym(nskLet, "size_")
-  let parallel_body = forEachContiguousImpl(
-    values, raw_ptrs, size, loopBody, omp_params
+  let body = forEachContiguousImpl(
+    values, raw_ptrs, size, loopBody, use_openmp, omp_params
     )
   let alias0 = aliases[0]
 
@@ -264,7 +263,52 @@ macro forEachContiguous*(args: varargs[untyped]): untyped =
       `test_shapes`
       `raw_ptrs_stmt`
       let `size` = `alias0`.size
-      `parallel_body`
+      `body`
+
+macro forEachContiguous*(args: varargs[untyped]): untyped =
+  ## Format:
+  ## forEachContiguous x in a, y in b, z in c, (512, 1024, true):
+  ##    x += y * z
+  ## (512, 1024, true) corresponds to omp_threshold, omp_grain_size, use_simd
+  ## from omp_parallel_for
+  forEachContiguousTemplate(true)
+
+macro forEachContiguousSerial*(args: varargs[untyped]): untyped =
+  ## Format:
+  ## forEachContiguousSerial x in a, y in b, z in c:
+  ##    x += y * z
+  ## OpenMP parameters will be ignored
+  forEachContiguousTemplate(false)
+
+template forEachStridedTemplate(use_openmp: static bool){.dirty.} =
+  var
+    params, loopBody, values, aliases, raw_ptrs: NimNode
+    aliases_stmt, raw_ptrs_stmt, test_shapes: NimNode
+    omp_params: NimNode
+
+  initForEach(
+        args,
+        params,
+        loopBody,
+        omp_params,
+        values, aliases, raw_ptrs,
+        aliases_stmt, raw_ptrs_stmt,
+        test_shapes
+  )
+
+  let size = genSym(nskLet, "size_")
+  let body = forEachStridedImpl(
+    values, aliases, raw_ptrs, size, loopBody, use_openmp, omp_params
+  )
+  let alias0 = aliases[0]
+
+  result = quote do:
+    block:
+      `aliases_stmt`
+      `test_shapes`
+      `raw_ptrs_stmt`
+      let `size` = `alias0`.size
+      `body`
 
 macro forEachStrided*(args: varargs[untyped]): untyped =
   ## Format:
@@ -275,34 +319,7 @@ macro forEachStrided*(args: varargs[untyped]): untyped =
   ##
   ## The OpenMP minimal per-core grain size
   ## is always scaled down by OMP_NON_CONTIGUOUS_SCALE_FACTOR (4 by default)
-  var
-    params, loopBody, values, aliases, raw_ptrs: NimNode
-    aliases_stmt, raw_ptrs_stmt, test_shapes: NimNode
-    omp_params: NimNode
-
-  initForEach(
-        args,
-        params,
-        loopBody,
-        omp_params,
-        values, aliases, raw_ptrs,
-        aliases_stmt, raw_ptrs_stmt,
-        test_shapes
-  )
-
-  let size = genSym(nskLet, "size_")
-  let parallel_body = forEachStridedImpl(
-    values, aliases, raw_ptrs, size, loopBody, true, omp_params
-  )
-  let alias0 = aliases[0]
-
-  result = quote do:
-    block:
-      `aliases_stmt`
-      `test_shapes`
-      `raw_ptrs_stmt`
-      let `size` = `alias0`.size
-      `parallel_body`
+  forEachStridedTemplate(true)
 
 macro forEachStridedSerial*(args: varargs[untyped]): untyped =
   ## Format:
@@ -310,49 +327,9 @@ macro forEachStridedSerial*(args: varargs[untyped]): untyped =
   ##    x += y * z
   ##
   ## Strided iteration with serial execution. OpenMP params passed to it will be ignored
-  var
-    params, loopBody, values, aliases, raw_ptrs: NimNode
-    aliases_stmt, raw_ptrs_stmt, test_shapes: NimNode
-    omp_params: NimNode
+  forEachStridedTemplate(false)
 
-  initForEach(
-        args,
-        params,
-        loopBody,
-        omp_params,
-        values, aliases, raw_ptrs,
-        aliases_stmt, raw_ptrs_stmt,
-        test_shapes
-  )
-
-  let size = genSym(nskLet, "size_")
-  let serial_body = forEachStridedImpl(
-    values, aliases, raw_ptrs, size, loopBody, false, omp_params
-  )
-  let alias0 = aliases[0]
-
-  result = quote do:
-    block:
-      `aliases_stmt`
-      `test_shapes`
-      `raw_ptrs_stmt`
-      let `size` = `alias0`.size
-      `serial_body`
-
-macro forEach*(args: varargs[untyped]): untyped =
-  ## Format:
-  ## forEach x in a, y in b, z in c, (512, 1024, true):
-  ##    x += y * z
-  ## (512, 1024, true) corresponds to omp_threshold, omp_grain_size, use_simd
-  ## from omp_parallel_for
-  ##
-  ## The iteration strategy is selected at runtime depending of
-  ## the tensors memory layout. If you know at compile-time that the tensors are
-  ## contiguous or strided, use forEachContiguous or forEachStrided instead.
-  ## Runtime selection requires duplicating the code body.
-  ##
-  ## If the tensors are non-contiguous, the OpenMP minimal per-core grain size
-  ## is scaled down by OMP_NON_CONTIGUOUS_SCALE_FACTOR (4 by default)
+template forEachTemplate(use_openmp: static bool) {.dirty.} =
   var
     params, loopBody, values, aliases, raw_ptrs: NimNode
     aliases_stmt, raw_ptrs_stmt, test_shapes: NimNode
@@ -370,10 +347,10 @@ macro forEach*(args: varargs[untyped]): untyped =
 
   let size = genSym(nskLet, "size_")
   let contiguous_body = forEachContiguousImpl(
-    values, raw_ptrs, size, loopBody, omp_params
+    values, raw_ptrs, size, loopBody, use_openmp, omp_params
   )
   let strided_body = forEachStridedImpl(
-    values, aliases, raw_ptrs, size, loopBody, true, omp_params
+    values, aliases, raw_ptrs, size, loopBody, use_openmp, omp_params
   )
   let alias0 = aliases[0]
   var test_C_Contiguous = newCall(ident"is_C_contiguous", alias0)
@@ -394,3 +371,32 @@ macro forEach*(args: varargs[untyped]): untyped =
         `contiguous_body`
       else:
         `strided_body`
+
+macro forEach*(args: varargs[untyped]): untyped =
+  ## Format:
+  ## forEach x in a, y in b, z in c, (512, 1024, true):
+  ##    x += y * z
+  ## (512, 1024, true) corresponds to omp_threshold, omp_grain_size, use_simd
+  ## from omp_parallel_for
+  ##
+  ## The iteration strategy is selected at runtime depending of
+  ## the tensors memory layout. If you know at compile-time that the tensors are
+  ## contiguous or strided, use forEachContiguous or forEachStrided instead.
+  ## Runtime selection requires duplicating the code body.
+  ##
+  ## If the tensors are non-contiguous, the OpenMP minimal per-core grain size
+  ## is scaled down by OMP_NON_CONTIGUOUS_SCALE_FACTOR (4 by default)
+  forEachTemplate(true)
+
+macro forEachSerial*(args: varargs[untyped]): untyped =
+  ## Format:
+  ## forEachSerial x in a, y in b, z in c:
+  ##    x += y * z
+  ##
+  ## openMP parameters will be ignored
+  ##
+  ## The iteration strategy is selected at runtime depending of
+  ## the tensors memory layout. If you know at compile-time that the tensors are
+  ## contiguous or strided, use forEachContiguousSerial or forEachStridedSerial instead.
+  ## Runtime selection requires duplicating the code body.
+  forEachTemplate(false)
