eb04b35dd4f6758bbdeee586ce3180d3664f7945
mratsim <mamy_github@numforge.co>
2018-10-24 12:33:59 +0200

    Example of using omp_parallel and omp_critical statement that doesn't work

--- examples/ex05_tensor_parallel_reduction.nim
+++ examples/ex05_tensor_parallel_reduction.nim
@@ -20,10 +20,8 @@ proc foo[T](x, y: Tensor[T]): T =
     partial_reduce[omp_get_thread_num() * padding] += xi + yi
 
   for idx in 0 ..< nb_chunks:
-    echo partial_reduce[idx * padding]
     result += partial_reduce[idx * padding]
-
-  echo partial_reduce
+  echo "foo partial_reduce: ", partial_reduce
 
 proc toTensor[T](s: seq[T]): Tensor[T] =
   var size: int
@@ -35,3 +33,26 @@ let a = toSeq(1..10001).toTensor
 let b = toSeq(-10000 .. 0).toTensor
 
 echo foo(a, b)
+
+# The following doesn't work because Nim OpenMP clause
+# uses "#pragma omp parallel for" directly
+# instead of "#pragma omp for"
+#
+# This means that each thread will do the same work
+# Final result will be nb_threads * real_result
+
+# proc foo_alt[T](x, y: Tensor[T]): T =
+#   const
+#     max_threads = 22
+#     cache_line = 64
+#     padding = cache_line div sizeof(T)
+#   withCompilerOptimHints()
+#   var nb_chunks: Natural
+#   omp_parallel:
+#     var local_sum = T(0)
+#     reduceEach nb_chunks, xi in x, yi in y:
+#       local_sum += xi + yi
+#     omp_critical:
+#       result += local_sum
+#
+# echo foo_alt(a, b)
