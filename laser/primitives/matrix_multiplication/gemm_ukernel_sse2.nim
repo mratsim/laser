84f48065e8e4aea55dae07b7536acb8efd40be5f
mratsim <mamy_github@numforge.co>
2018-11-17 18:56:15 +0100

    Use scalar code for int32 if no SSE4.1

--- laser/primitives/matrix_multiplication/gemm_ukernel_sse2.nim
+++ laser/primitives/matrix_multiplication/gemm_ukernel_sse2.nim
@@ -25,23 +25,60 @@ ukernel_generator(
       simd_fma = float64x2_muladd_unfused
     )
 
-template int32x4_muladd_unfused_sse2(a, b, c: m128i): m128i =
-  mm_add_epi32(int32x4_mul_sse2_fallback(a, b), c)
+#######################################
+#
+# Int32: hack to unroll scalar code
+#
+#######################################
+
+# This is faster than using the fallback for mm_mullo_epi32
+# in laser/primitives/private/sse2_utils
+
+# Note that we are quite limited in registers with scalar code
+# as those are competing with all loop controls, conditions, ...
+
+type Int32x2 = array[2, int32]
+
+func setZero_int32_sse2_fallback(): Int32x2 {.inline.} =
+  discard
+
+template set1_int32_sse2_fallback(a: int32): Int32x2 =
+  [a, a]
+
+func load_int32_sse2_fallback(mem_addr: ptr int32): Int32x2 {.inline.}=
+  let p = cast[ptr UncheckedArray[int32]](mem_addr)
+  [p[0], p[1]]
+
+func store_int32_sse2_fallback(mem_addr: ptr int32, a: Int32x2) {.inline.}=
+  let p = cast[ptr UncheckedArray[int32]](mem_addr)
+  p[0] = a[0]
+  p[1] = a[1]
+
+template add_int32_sse2_fallback(a, b: Int32x2): Int32x2 =
+  [a[0] + b[0], a[1] + b[1]]
+
+template mul_int32_sse2_fallback(a, b: Int32x2): Int32x2 =
+  [a[0] * b[0], a[1] * b[1]]
+
+template fma_int32_sse2_fallback(a, b, c: Int32x2): Int32x2 =
+  ## By mistake I had c[0] instead of c[1] and twice the speed
+  [c[0] + a[0]*b[0], c[1] + a[1]*b[1]]
 
 ukernel_generator(
       x86_SSE2,
       typ = int32,
-      vectype = m128i,
-      nb_scalars = 4,
-      simd_setZero = mm_setzero_si128,
-      simd_broadcast_value = mm_set1_epi32,
-      simd_load_aligned = mm_load_si128,
-      simd_load_unaligned = mm_loadu_si128,
-      simd_store_unaligned = mm_storeu_si128,
-      simd_mul = int32x4_mul_sse2_fallback,
-      simd_add = mm_add_epi32,
-      simd_fma = int32x4_muladd_unfused_sse2
+      vectype = Int32x2,
+      nb_scalars = 2,
+      simd_setZero = setZero_int32_sse2_fallback,
+      simd_broadcast_value = set1_int32_sse2_fallback,
+      simd_load_aligned = load_int32_sse2_fallback,
+      simd_load_unaligned = load_int32_sse2_fallback,
+      simd_store_unaligned = store_int32_sse2_fallback,
+      simd_mul = mul_int32_sse2_fallback,
+      simd_add = add_int32_sse2_fallback,
+      simd_fma = fma_int32_sse2_fallback
     )
+
 
 #######################################
 #
