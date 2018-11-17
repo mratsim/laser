910591ffdaa072ecf8fa65ccbfbe791b69f8371d
mratsim <mamy_github@numforge.co>
2018-11-17 14:11:03 +0100

    small fix if case of gemm

--- laser/primitives/matrix_multiplication/gemm.nim
+++ laser/primitives/matrix_multiplication/gemm.nim
@@ -199,9 +199,9 @@ proc gemm_strided*[T: SomeNumber](
     when defined(i386) or defined(amd64):
       when T is float32:
         if cpuinfo_has_x86_fma3():   dispatch(x86_AVX_FMA)
-        if cpuinfo_has_x86_avx():  dispatch(x86_AVX)
+        elif cpuinfo_has_x86_avx():  dispatch(x86_AVX)
         elif cpuinfo_has_x86_sse():    dispatch(x86_SSE)
-      when T is float64:
+      elif T is float64:
         if cpuinfo_has_x86_fma3():   dispatch(x86_AVX_FMA)
         elif cpuinfo_has_x86_avx():  dispatch(x86_AVX)
         elif cpuinfo_has_x86_sse2():    dispatch(x86_SSE2)
