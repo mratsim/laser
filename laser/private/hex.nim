5186d43afe301197aa332d0a8725446c0b72abe3
mratsim <mamy_github@numforge.co>
2018-11-29 22:52:04 +0100

    Add a toHex routine for debugging

---
+++ laser/private/hex.nim
@@ -0,0 +1,13 @@
+# Laser
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+func toHex*(bytes: openarray[byte]): string =
+  const hexChars = "0123456789abcdef"
+
+  result = newString(3 * bytes.len)
+  for i in 0 ..< bytes.len:
+    result[3*i  ] = hexChars[int bytes[i] shr 4 and 0xF]
+    result[3*i+1] = hexChars[int bytes[i]       and 0xF]
+    result[3*i+2] = ' '
