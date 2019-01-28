5179aa76f91db9c7324678629cc43d5067d7a7ba
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-01-28 12:11:08 +0100

    initial impl of fast exponentials

---
+++ laser/primitives/simd_math/exp_log_common.nim
@@ -0,0 +1,45 @@
+# Laser
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+# See:
+#  - exp_and_log_optimisation_resources.md
+#  - https://github.com/herumi/fmath
+#
+#  - http://www.convict.lu/Jeunes/ultimate_stuff/exp_ln_2.htm
+#  - http://soc.knu.ac.kr/video_lectures/15_4.pdf
+#  - http://arith.cs.ucla.edu/publications/LogExp-TC04.pdf
+#    - Range reduction + LookUp Table approximation
+#
+#  - https://nic.schraudolph.org/pubs/Schraudolph99.pdf
+#    - Fast exp approximation using IEEE754 logarithmic nature
+
+import math
+
+# ############################################################
+#
+#                     Float32 Exponentiation
+#
+# ############################################################
+
+const
+  ExpBits* = 10
+  ExpBitsMask* = 1 shl ExpBits
+
+  MantissaBits* = 23
+  MantissaBitsMask* = 1 shl 23
+
+  ln2 = ln(2'f32)
+
+  ExpMax* = 88'i32
+  ExpMin* = -88'i32
+  ExpA* = ExpBitsMask.float32 / ln2
+  ExpB* = ln2 / ExpBitsMask.float32
+
+func initExpLUT(): array[ExpBitsMask, int32] =
+  for i, val in result.mpairs:
+    let y = pow(2'f32, i.float32 / ExpBitsMask.float32)
+    val = cast[int32](y) and (MantissaBitsMask - 1)
+
+let ExpLUT* = initExpLUT()
---
+++ laser/primitives/simd_math/exp_log_fallback.nim
@@ -0,0 +1,49 @@
+# Laser
+# Copyright (c) 2018 Mamy André-Ratsimbazafy
+# Distributed under the Apache v2 License (license terms are at http://www.apache.org/licenses/LICENSE-2.0).
+# This file may not be copied, modified, or distributed except according to those terms.
+
+# See:
+#  - exp_and_log_optimisation_resources.md
+#  - https://github.com/herumi/fmath
+#
+#  - http://www.convict.lu/Jeunes/ultimate_stuff/exp_ln_2.htm
+#  - http://soc.knu.ac.kr/video_lectures/15_4.pdf
+#  - http://arith.cs.ucla.edu/publications/LogExp-TC04.pdf
+#    - Range reduction + LookUp Table approximation
+#
+#  - https://nic.schraudolph.org/pubs/Schraudolph99.pdf
+#    - Fast exp approximation using IEEE754 logarithmic nature
+
+import
+  math,
+  ./exp_log_common
+
+# ############################################################
+#
+#          Float32 AVX2 vectorized Exponentiation
+#
+# ############################################################
+
+proc fast_exp(x: float32): float32 =
+
+  static: assert cpuEndian == littleEndian, "Fast exp has not been tested on big-endian architectures"
+
+  let r = int32(x * ExpA) # ->f32->i32->f32 Rounding is important
+  let t = x - r.float32 * ExpB + 1
+
+  let v = r and (ExpBitsMask - 1)
+  var u = r + (127 shl ExpBits)
+  u = u shr ExpBits
+  u = u shl MantissaBits
+
+  let ti = ExpLut[v] or u
+  result = t * cast[float32](ti)
+
+when isMainModule:
+
+  echo exp(0.5'f32)
+  echo fast_exp(0.5'f32)
+
+  echo exp(-0.5'f32)
+  echo fast_exp(-0.5'f32)
